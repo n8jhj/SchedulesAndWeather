e1fbd82d8f194bf4fd3faa00a0c051f8d657e8fd
Nathaniel Jones <n8jhj@live.com>
2017-07-11 17:31:14 -0700

    initial commit
    First building struct created - SmallOffice.mat.
    Import weather file data function created.

---
+++ importWeatherDataToStruct.m
@@ -0,0 +1,48 @@
+function wStruct = importWeatherDataToStruct(zone)
+%IMPORTWEATHERDATATOSTRUCT Import weather data from .epw file to MATLAB
+%struct.
+
+%% Get to EPW files directory
+origDir = pwd;
+cd 'C:\\Users\Admin\Documents\EnergyPlus\Climate Zones - TMY3'
+dirList = dir;
+
+%% Get to proper zone directory
+for d = 1:1:length(dirList)
+    thisDir = dirList(d).name;
+    thisZone = strtok(thisDir);
+    if thisZone == zone
+        cd(thisDir)
+        break
+    end
+end
+zoneDir = dir;
+
+%% Import
+for d = 1:1:length(zoneDir)
+    thisDir = zoneDir(d).name;
+    if length(thisDir) > 3 && strcmp(thisDir(end-3:end), '.epw')
+        filename = thisDir;
+        break
+    end
+end
+assert(exist('filename','var')==1, 'File name not defined')
+[yr,mo,dy,hr,mn,tDB,tWB,relHum] = importWeatherData(filename,9,Inf);
+
+%% Get timestamp list
+timestamp = datetime(num2str([yr,mo,dy,hr,mn]), ...
+    'InputFormat','yyyy     M     d     H     m');
+
+%% Create return struct
+wStruct = struct(...
+'zone',zone,...
+'timestamp',timestamp,...
+'tdb',tDB,...
+'twb',tWB,...
+'relHum',relHum...
+);
+
+%% Change back to original directory
+cd(origDir)
+
+end
